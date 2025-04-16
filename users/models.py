20f5d7a2d206f10ca1d4da0f0d1037f6b090c566
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-04-16 12:25:56 +0200

    add-models

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.username
