1800689c1ee8671064bb9573b343fee4ba1417a3
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-04-16 12:30:51 +0200

    add-models in analytics

---
+++ analytics/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from posts.models import Post
+
+
+class PostStat(models.Model):
+    post = models.OneToOneField(Post, on_delete=models.CASCADE)
+    like_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
+    share_count = models.IntegerField(default=0)
+    engagement_rate = models.FloatField(default=0)
+
+    def update_stats(self):
+        self.like_count = self.post.like_set.count()
+        self.comment_count = self.post.comment_set.count()
+        self.save()
+        self.calculate_engagement()
+
+    def calculate_engagement(self):
+        # Basic engagement rate calculation
+        from users.models import User
+        total_users = User.objects.count()
+        if total_users > 0:
+            self.engagement_rate = (self.like_count + self.comment_count) / total_users * 100
+            self.save()
+
+    def __str__(self):
+        return f"Stats for {self.post}"
