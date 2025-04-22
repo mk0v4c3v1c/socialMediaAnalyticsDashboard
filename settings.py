42395be146850885567f47c0c78c318d9653e61d
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-04-22 20:18:09 +0200

    add auth jwt-settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+INSTALLED_APPS = [
+    ...,
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'users',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
