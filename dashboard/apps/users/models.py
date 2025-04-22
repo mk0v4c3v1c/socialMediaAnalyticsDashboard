f2f4574f92a4cece3ccc1213bc368add40628e2f
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-04-22 20:19:08 +0200

    update users-models

--- dashboard/apps/users/models.py
+++ dashboard/apps/users/models.py
@@ -6,6 +6,7 @@ class User(AbstractUser):
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=100, blank=True)
     website = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
 
     def __str__(self):
         return self.username
