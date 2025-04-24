c431bce13e99fdcb29d75738a48effa1f06fb99b
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-04-24 16:05:31 +0200

    update analytics-models

--- dashboard/apps/analytics/models.py
+++ dashboard/apps/analytics/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from dashboard.apps.posts.models import Post
-
+from dashboard.apps.users.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class PostStat(models.Model):
     post = models.OneToOneField(Post, on_delete=models.CASCADE)
@@ -10,18 +12,24 @@ class PostStat(models.Model):
     engagement_rate = models.FloatField(default=0)
 
     def update_stats(self):
-        self.like_count = self.post.like_set.count()
-        self.comment_count = self.post.comment_set.count()
-        self.save()
+        # Get all counts in single query
+        stats = self.post.like_set.count(), self.post.comment_set.count()
+        self.like_count, self.comment_count = stats
         self.calculate_engagement()
+        self.save()
 
     def calculate_engagement(self):
-        # Basic engagement rate calculation
-        from dashboard.apps.users.models import User
         total_users = User.objects.count()
         if total_users > 0:
             self.engagement_rate = (self.like_count + self.comment_count) / total_users * 100
-            self.save()
+        else:
+            self.engagement_rate = 0
 
     def __str__(self):
         return f"Stats for {self.post}"
+
+# Move signal handler outside the class
+@receiver(post_save, sender=Post)
+def create_post_stat(sender, instance, created, **kwargs):
+    if created:
+        PostStat.objects.create(post=instance)
